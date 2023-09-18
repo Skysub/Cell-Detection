e2cfccfd8c618486400d01be62b356443b652c9e
Freddy <frederik.hedeandersen@gmail.com>
2023-09-19 01:31:17 +0200

    Adds support for processing an entire folder at once (up to 100 images)

--- Cell Detection/main.c
+++ Cell Detection/main.c
@@ -36,10 +36,9 @@ int main(int arcg, char **argv)
     printf("%s \n", argv[1]);
     printf("%s \n", argv[2]);
 
-    clock_t startProgram, endProgram, startProcessing, endProcessing;
-    double cpu_time_used_program, cpu_time_used_processing;
+    clock_t startProgram, endProgram;
+    double cpu_time_used_program;
     startProgram = clock();
-    startProcessing = clock();
 #endif
 
     if (arcg < 2)
@@ -51,8 +50,9 @@ int main(int arcg, char **argv)
     }
 
     //File list for folder mode
-    char file_list[100][40];
+    char file_list[100][60];
     int file_list_length = 0;
+    char output_folder[] = "output/";
 
     //Check if the input argument is a folder or a file
     struct stat buf;
@@ -63,12 +63,25 @@ int main(int arcg, char **argv)
         //Prints an error message and exits if the directory could not be opened
         if(getImagePathsFromFolder(argv[1], file_list, &file_list_length)) exit(1);
     }
+    else file_list_length++;
+
+    //Initiates the for loop for processing all the file in the older
+    //If folder mode is not active, then the for loop runs once
+    for (int f = 0; f < file_list_length; f++)
+    {
+#if _DEBUG_FOLDER
+        printf("\n");
+        printf(file_list[f]);
+        printf("\n");
+#endif
 
     char img_name[60] = ""; //The buffer is larger than the names we allow the input file to have
                             //This is because we add a suffix to this string later
     if (folderMode) {
-        printf("Folder Mode not yet implemented");
-        exit(69);
+        //The name length is guaranteed in folderMode
+        int folder_len = strlen(argv[1]) + 1;
+        int len = strlen(file_list[f]) - 4;
+        memcpy(img_name, file_list[f] + folder_len, len - folder_len); //Only adding the name part
     }
     else {
         //Copies the name of the input file into img_name (no file extension)
@@ -77,6 +90,12 @@ int main(int arcg, char **argv)
         memcpy(img_name, argv[1], len);
     }
 
+#if _DEBUG
+    clock_t startProcessing, endProcessing;
+    double cpu_time_used_processing;
+    startProcessing = clock();
+#endif
+
 #if OUTPUT_INTERMEDIARY_IMAGES
     //This code block runs only for testing purposes
 
@@ -84,8 +103,14 @@ int main(int arcg, char **argv)
     char grey_suffix[] = "_grey.bmp";
     char binary_suffix[] = "_binary.bmp";
 
-    char grey_name[60] = "";
-    char binary_name[60] = "";
+    char grey_name[80] = "";
+    char binary_name[80] = "";
+
+    if (folderMode) {
+        //Remember to output the files in the output folder
+        strcat(grey_name, output_folder);
+        strcat(binary_name, output_folder);
+    }
 
     strcat(grey_name, img_name);
     strcat(grey_name, grey_suffix);
@@ -93,7 +118,10 @@ int main(int arcg, char **argv)
     strcat(binary_name, binary_suffix);
 
     //loads the image file
-    read_bitmap(argv[1], input_image);
+    if (folderMode) { 
+        read_bitmap(file_list[f], input_image);
+    }
+        else read_bitmap(argv[1], input_image);
     convert_to_gray(input_image, buff1_image);
 
     //Output the greyscale version
@@ -108,7 +136,10 @@ int main(int arcg, char **argv)
 
     copy_bmp(buff1_image, buff2_image);
 #else
-    read_bitmap(argv[1], input_image);
+    if (folderMode) {
+        read_bitmap(file_list[f], input_image);
+    }
+    else read_bitmap(argv[1], input_image);
     convert_to_gray(input_image, buff1_image);
     convert_to_binary_image(threshold, buff1_image);
     copy_bmp(buff1_image, buff2_image);
@@ -131,33 +162,34 @@ int main(int arcg, char **argv)
     while (true)
     {
 #if _DEBUG
-        printf("%d \n", i++); //Prints the amount of times the loop has run
-#endif _DEBUG
+        printf("%d ", i++); //Prints the amount of times the loop has run
+#endif
 
-        if (erode(buff1_image, buff2_image)){
+        if (erode(buff1_image, buff2_image)) {
             break;
         }
         count += detectCellsIterator(buff2_image, cell_list, &cell_list_length);
         copy_bmp(buff2_image, buff1_image);
     }
 #if _DEBUG
+    printf("\n");
     endLoop = clock();
 #endif
     printf("%d \n", count); //Prints the final cell count
 
     for (short i = 0; i < cell_list_length; i++)
     {
-#if !_DEBUG || PRINT_CELL_LIST_IN_DEBUG
+#if (!_DEBUG || PRINT_CELL_LIST_IN_DEBUG) && !NDEBUG_FOLDER
         printf("(%d, %d)\n", cell_list[i][0], cell_list[i][1]);
 #endif
         draw_red_cross(input_image, cell_list[i][0], cell_list[i][1]);
     }
-    
+
     char out_name[60] = "";
     char suffix[] = "_output.bmp"; //default suffix
 
     //Choosing a name for the output file based on whether there is given a name/suffix for the output file/files
-    if (arcg < 3){
+    if (arcg < 3) {
         strcat(img_name, suffix);
         strcat(out_name, img_name);
     }
@@ -165,27 +197,37 @@ int main(int arcg, char **argv)
         strcat(img_name, argv[2]);
         strcat(out_name, img_name);
     }
-    else 
+    else
         strcat(out_name, argv[2]);
-        
+
     //outputs the final processed image
-    write_bitmap(input_image, out_name);
-        
+    if (folderMode) {
+        char folder_out_name[70] = "";
+        strcat(folder_out_name, output_folder);
+        strcat(folder_out_name, out_name);
+        write_bitmap(input_image, folder_out_name);
+    }
+    else write_bitmap(input_image, out_name);
+
+#if _DEBUG
+    cpu_time_used_processing = endProcessing - startProcessing;
+    cpu_time_used_loop = endLoop - startLoop;
+    printf("Img proprocesseing time: %f ms\n", cpu_time_used_processing * 1000.0 / CLOCKS_PER_SEC);
+    printf("Algorithm running time:  %f ms\n", cpu_time_used_loop * 1000.0 / CLOCKS_PER_SEC);
+#endif
+
+    }
 
 #if _DEBUG
     endProgram = clock();
     cpu_time_used_program = endProgram - startProgram;
-    cpu_time_used_processing = endProcessing - startProcessing;
-    cpu_time_used_loop = endLoop - startLoop;
-    printf("Total program time: %f ms\n", cpu_time_used_program * 1000.0 / CLOCKS_PER_SEC);
-    printf("Total image proprocesseing time: %f ms\n", cpu_time_used_processing * 1000.0 / CLOCKS_PER_SEC);
-    printf("Total algorithm runtime: %f ms\n", cpu_time_used_loop * 1000.0 / CLOCKS_PER_SEC);
+    printf("\nTotal program time: %f ms\n", cpu_time_used_program * 1000.0 / CLOCKS_PER_SEC);
 #endif
 
     return 0;
 }
 
-int getImagePathsFromFolder(const char folder[], char file_list[100][40], int* file_list_length) {
+int getImagePathsFromFolder(const char folder[], char file_list[100][60], int* file_list_length) {
 
     DIR *dir;
     struct dirent* item;
@@ -211,10 +253,21 @@ int getImagePathsFromFolder(const char folder[], char file_list[100][40], int* f
            item->d_name[item->d_namlen - 1] == 'p' &&
            *file_list_length < 100) {
 
-            strcpy(file_list[*file_list_length], item->d_name);
+            char temp[60] = "";
+            strcat(temp, folder);
+            strcat(temp, "/");
+            strcat(temp, item->d_name);
+            strcpy(file_list[*file_list_length], temp);
             (*file_list_length)++;
         }
     }
     closedir(dir);
+
+    //creates the output folder if it doesn't already exist
+    struct stat st;
+    if (stat("output", &st) == -1) {
+        mkdir("output", 0777);
+    }
+
     return 0;
 }
